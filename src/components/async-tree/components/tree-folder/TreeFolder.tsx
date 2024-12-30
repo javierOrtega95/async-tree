7aab3bbefae5fe1c64a9f1dca2f5977c2364d1d4
javierOrtega95 <fjortega@wizzie.io>
2024-12-30 22:00:11 +0100

    refactor: simplify default TreeItem and TreeFolder components

--- src/components/async-tree/components/tree-folder/TreeFolder.tsx
+++ src/components/async-tree/components/tree-folder/TreeFolder.tsx
@@ -8,7 +8,12 @@ export default function TreeFolder({
   onClick,
 }: FolderProps): JSX.Element {
   return (
-    <div className='tree-folder' onClick={() => onClick(node)}>
+    <div
+      id={node.id}
+      data-testid={node.id}
+      className='tree-folder'
+      onClick={() => onClick(node)}
+    >
       {isLoading && (
         <svg
           width='12'
@@ -40,20 +45,6 @@ export default function TreeFolder({
           <path d='m9 18 6-6-6-6'></path>
         </svg>
       )}
-
-      <svg
-        width='24'
-        height='24'
-        viewBox='0 0 24 24'
-        fill='none'
-        stroke='currentColor'
-        strokeWidth='2'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        className='icon folder-icon'
-      >
-        <path d='M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z'></path>
-      </svg>
 
       <span className='folder-name'>{node.name}</span>
     </div>
--- src/components/async-tree/components/tree-item/TreeItem.tsx
+++ src/components/async-tree/components/tree-item/TreeItem.tsx
@@ -3,23 +3,7 @@ import './TreeItem.css'
 
 export default function TreeItem({ node }: ItemProps): JSX.Element {
   return (
-    <div className='tree-item'>
-      <svg
-        width='24'
-        height='24'
-        viewBox='0 0 24 24'
-        fill='none'
-        color='#6b7280'
-        stroke='currentColor'
-        strokeWidth='2'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        className='icon file-icon'
-      >
-        <path d='M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z'></path>
-        <path d='M14 2v4a2 2 0 0 0 2 2h4'></path>
-      </svg>
-
+    <div id={node.id} data-testid={node.id} className='tree-item'>
       <span>{node.name}</span>
     </div>
   )
